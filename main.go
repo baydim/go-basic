fac1a8643980132d0816a2d86c594256e2305261
baydim <baydim56@gmail.com>
2022-09-05 16:29:04 +0700

    initial

--- main.go
+++ main.go
@@ -18,5 +18,18 @@ func main() {
 		)
 	})
 
+	router.GET("/fish", getFish)
+	router.GET("/fish/:id", getFishQ)
+
 	router.Run(":8080")
+}
+
+func getFish(c *gin.Context) {
+	id := c.Query("id")
+	c.JSON(http.StatusOK, gin.H{"message": "pong", "id": id})
+}
+
+func getFishQ(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "pong", "id": id})
 }
