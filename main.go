31c06463f4a9d020f2d1d2f0184fa060e21af5bd
baydim <baydim56@gmail.com>
2022-09-05 16:43:05 +0700

    initial

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 		c.JSON(http.StatusOK,
 
 			gin.H{
-				"message": "pong",
+				"message": "ping pong",
 			},
 		)
 	})
-
 	router.GET("/fish", getFish)
 	router.GET("/fish/:id", getFishQ)
+	router.POST("/fish", postFish)
 
 	router.Run(":8080")
 }
@@ -32,4 +32,16 @@ func getFish(c *gin.Context) {
 func getFishQ(c *gin.Context) {
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{"message": "pong", "id": id})
+}
+
+type FishModel struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func postFish(c *gin.Context) {
+	var fish FishModel
+	c.BindJSON(&fish)
+	c.JSON(http.StatusOK, gin.H{"message": "pong", "fish": fish})
+
 }
