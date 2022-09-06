199dcc3d7d37fb19d3701cbc06ee4acb32577948
baydim <baydim56@gmail.com>
2022-09-06 15:10:44 +0700

    learn today

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK,
 
@@ -20,8 +24,10 @@ func main() {
 	router.GET("/fish", getFish)
 	router.GET("/fish/:id", getFishQ)
 	router.POST("/fish", postFish)
-
-	router.Run(":8080")
+	router.POST("/upload", uploadSingleFile)
+	router.GET("/delete/:filename", deleteSingleFile)
+	router.GET("/fetch/:id", fetchApi)
+	router.Run(":1234")
 }
 
 func getFish(c *gin.Context) {
@@ -43,5 +49,46 @@ func postFish(c *gin.Context) {
 	var fish FishModel
 	c.BindJSON(&fish)
 	c.JSON(http.StatusOK, gin.H{"message": "pong", "fish": fish})
+}
 
+func uploadSingleFile(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	c.SaveUploadedFile(file, file.Filename)
+	filename := filepath.Base(file.Filename)
+	response := c.SaveUploadedFile(file, filename)
+	if response == nil {
+		c.JSON(http.StatusOK, gin.H{"message": "pong", "filename": filename})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "pong", "filename": filename})
+	}
+}
+
+func deleteSingleFile(c *gin.Context) {
+	response := os.Remove(c.Param("filename"))
+	if response == nil {
+		c.JSON(http.StatusOK, gin.H{"message": "pong", "filename": c.Param("filename")})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "pong", "filename": c.Param("filename")})
+	}
+}
+
+func fetchApi(c *gin.Context) {
+	id := c.Params.ByName("id")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/" + id)
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "pong", "error": err})
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "pong", "error": err})
+	}
+	//Convert the body to type string
+	sb := json.RawMessage(body)
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":      id,
+		"message": "pong", "body": sb})
 }
